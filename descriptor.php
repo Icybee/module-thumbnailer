25afc56a52bb88ed843da80eb882abe2f0553b19
Olivier Laviale <contact@weirdog.com>
2014-03-18 16:04:58 +0100

    Added 'registry' to the dependencies

--- descriptor.php
+++ descriptor.php
@@ -10,5 +10,9 @@ return array
 	Module::T_TITLE => 'Thumbnailer',
 	Module::T_DESCRIPTION => 'Create thumbnails on the fly',
 	Module::T_NAMESPACE => __NAMESPACE__,
-	Module::T_PERMISSION => false
+	Module::T_PERMISSION => false,
+	Module::T_REQUIRES => array
+	(
+		"registry" => "1.x"
+	)
 );
